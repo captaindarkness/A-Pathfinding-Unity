fa98150d2c2535d464bcda8f20c6743cfb7f584d
captaindarkness <jonathanstrandbech@gmail.com>
2020-02-22 14:53:24 +0100

    Added base files

---
+++ EnemyUnit.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.AI;
+
+public class EnemyUnit : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent agent;
+    //public Transform target;
+    public GameObject target;
+    public Vector3 targetPos;
+    public float distanceFromPlayer;
+	public Image healthBar;
+    float speed = 3;
+    Vector3[] path;
+    int targetIndex;
+	public int maxHealth;
+	public int health;
+
+    void Start()
+    {
+        //PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+		maxHealth = 100;
+		health = maxHealth;
+    }
+
+	void Update()
+	{
+		// if (Input.GetKeyDown(KeyCode.LeftControl)) {
+		// 	PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+		// }       
+        targetPos = target.transform.position;
+        distanceFromPlayer = Vector3.Distance(this.transform.position, targetPos);
+        //Go to Player when "E" is pressed
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            agent.SetDestination(targetPos);
+        }
+        //Go to point clicked on screen
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+
+	void TakeDamage(int damage)
+	{
+		// health -= damage;
+
+		// healthBar.fillAmount = (float)health / (float)maxHealth;
+
+		// if (health <= 0) {
+		// 	print ("dead");
+		// }
+	}
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+					print ("Movement DONE!");
+					targetIndex = 0;
+					path = new Vector3[0];
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector3.one * 0.3f);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Pathfinding.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class Pathfinding : MonoBehaviour {
+
+    PathRequestManager requestManager;
+
+    Grid grid;
+
+    void Awake()
+    {
+        requestManager = GetComponent<PathRequestManager>();
+        grid = GetComponent<Grid>();
+    }
+    #region Find path via button press
+    //void Update()
+    //{
+    //    if (Input.GetButtonDown("Jump"))
+    //    {
+    //        FindPath(seeker.position, target.position);
+    //    } 
+    //}
+    #endregion
+    public void StartFindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        StartCoroutine(FindPath(startPos, targetPos));
+    }
+
+    IEnumerator FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        Vector3[] waypoints = new Vector3[0];
+        bool pathSuccess = false;
+
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        //Only find the path if both are on walkable nodes. eg. not in a wall.
+        if (startNode.walkable && targetNode.walkable)
+        {
+            //v - Simple method but not Optimized for many paths - v
+            //List<Node> openSet = new List<Node>();
+
+            //Using the Heap Class for Optimization - v
+            Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+            HashSet<Node> closedSet = new HashSet<Node>();
+            openSet.Add(startNode);
+
+            while (openSet.Count > 0)
+            {
+                #region Unoptimized method
+                //v - Simple method but not Optimized for many paths - v
+                //Node currentNode = openSet[0];
+                //for (int i = 1; i < openSet.Count; i++)
+                //{
+                //    if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                //    {
+                //        currentNode = openSet[i];
+                //    }
+                //}
+
+                //openSet.Remove(currentNode);
+                #endregion
+                //Using Heap for Optimization - v
+                Node currentNode = openSet.RemoveFirst();
+                closedSet.Add(currentNode);
+
+                if (currentNode == targetNode)
+                {
+                    //!Path Found!
+                    sw.Stop();
+                    print("Path found in: " + sw.ElapsedMilliseconds + " ms");
+                    pathSuccess = true;
+                    break;
+                }
+
+                foreach (Node neighbour in grid.GetNeighbours(currentNode))
+                {
+                    if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                    if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                    {
+                        neighbour.gCost = newMovementCostToNeighbour;
+                        neighbour.hCost = GetDistance(neighbour, targetNode);
+                        neighbour.parent = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                        {
+                            openSet.Add(neighbour);
+                        }
+                        else
+                        {
+                            openSet.UpdateItem(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+        yield return null;
+        if (pathSuccess)
+        {
+            waypoints = RetracePath(startNode, targetNode);
+        }
+        requestManager.FinishedProcessingPath(waypoints, pathSuccess);
+    }
+
+    Vector3[] RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        Vector3[] waypoints = SimplifyPath(path);
+        Array.Reverse(waypoints);
+        return waypoints;
+    }
+
+    Vector3[] SimplifyPath(List<Node> path)
+    {
+        List<Vector3> waypoints = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
+
+        for (int i = 1; i < path.Count; i++)
+        {
+            Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+            if (directionNew != directionOld)
+            {
+                waypoints.Add(path[i].worldPosition);
+            }
+            directionOld = directionNew;
+        }
+        return waypoints.ToArray();
+    }
+
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int distX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distY = Mathf.Abs(nodeB.gridY - nodeB.gridY);
+
+        if (distX > distY)
+            return 14 * distY + 10 * (distX - distY);
+        return 14 * distX + 10 * (distY - distX);
+    }
+}
